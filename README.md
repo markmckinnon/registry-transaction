7cd6827b297627f416012c3431c999445a6a6ca7
Mark McKinnon <Mark.McKinnon13@outlook.com>
2025-06-13 22:10:26 -0400

    Initial commit
    Initial commit of the code

---
+++ README.md
@@ -0,0 +1,25 @@
+# Registry-Transactions
+
+## Summary
+This program will apply the registry hive transaction logs to the registry hive.
+
+## Requirements
+This project requires several additional modules. You can install them with the provided requirements.txt file as follows:
+
+```bash
+python3 -m pip install -r requirements.txt
+```
+This has been tested on Pythin 3.9.
+
+## Command Line
+![cmd_help](./images/command-line-options.png)
+
+If the secondary transaction log file is zero (0) bytes in lenght then it will be ignored
+
+If the primary transaction log file is zero (0) bytes in length the program will exit and not update the registry hive
+
+
+## Need Help?
+If you have any questions/comments/suggestions please let me know.  [Create an issue](https://github.com/markmckinnon/Autopsy-Plugins/issues/new).  Enjoy!
+
+
---
+++ registry-transaction.py
@@ -0,0 +1,76 @@
+# registry-transaction.py  - Program to apply the transaction log(s) to a registry hive.
+#
+# Copyright (C) 2025 Mark McKinnon (Mark dot McKinnon at Gmail dot com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You can view the GNU General Public License at <http://www.gnu.org/licenses/>
+#
+# Version History:
+#  Initial Version - 1.0 - June 2025
+#
+
+from regipy.recovery import apply_transaction_logs
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Apply Registry Transaction Logs to Registry Hive.")
+    parser.add_argument("-r", "--registryhive", type=str, action="store",
+                        help="Path and name of registry hive")
+    parser.add_argument("-p", "--primarytransactionlog", type=str, action="store",
+                        help="Path and Name of the Primary Transaction Log")
+    parser.add_argument("-s", "--secondarytransactionlog", type=str, action="store",
+                        help="Path and Name of the Secondary Transaction Log")
+    args = parser.parse_args()
+
+    if args.registryhive is None:
+        print ("Path and name of the registry hive was not specified")
+        return
+    if args.primarytransactionlog is None:
+        print ("Path and name of the primary transaction log was not specified")
+        return
+
+    primaryTransactionSize = os.path.getsize(args.primarytransactionlog)
+    if primaryTransactionSize != 0:
+        print(f"Primary Transaction File size: {primaryTransactionSize} bytes")
+    else:
+        print(f"Primary Transaction File size is 0 bytes, No transactions to process")
+        return
+
+    if args.secondarytransactionlog is not None:
+        secondaryTransactionSize = os.path.getsize(args.secondarytransactionlog)
+        if secondaryTransactionSize != 0:
+            print(f"secondary Transaction File size: {secondaryTransactionSize} bytes")
+            secondaryTransactionFile = args.secondarytransactionlog
+        else:
+            print(f"Secondary Transaction File size is 0 bytes, No secondary transactions to process")
+            secondaryTransactionFile = None
+    else:
+        secondaryTransactionFile = None
+
+    dirtyHive = args.registryhive + "_dirty"
+    os.rename(args.registryhive, dirtyHive)
+
+    # Recover hive
+    try:
+       restoredHivePath, recoverdDirtyPages = apply_transaction_logs(dirtyHive, args.primarytransactionlog, secondaryTransactionFile, args.registryhive)
+       os.remove(dirtyHive)
+       print(f"Registry Hive {args.registryhive} Recovered successfully")
+       print(f"Registry Hive restored to {restoredHivePath}")
+       print("Number of recovered dirty pages is " + str(recoverdDirtyPages))
+    except:
+       print ("Failed to apply transaction logs")
+       os.rename(dirtyHive, args.registryhive)
+
+if __name__ == '__main__':
+    main()
